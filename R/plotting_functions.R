47b9a8456a8f33298c42108834e78b5ad4d320c1
pschretlen <peter.schretlen@gmail.com>
2015-04-09 23:06:06 -0400

    adding support for plotting multiple measures; tweak plotting function; update example images in readme.

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -6,14 +6,21 @@ diff_plot <- function(data, debug = F ) {
   #flatten any missing slopes
   plot_data$slope <- replace(plot_data$slope, is.na(plot_data$slope), 0)
   
+  max_y = round( max(plot_data$rank), -1 )
+  y_breaks <- seq( -max_y, 0, 5 )
+  y_labels <- as.character( seq( max_y, 0, -5 ) )
+  
   plot <- ggplot(data = plot_data, aes(x = measurement_id, y = -rank)) + 
     adjust_theme( debug ) +
     scale_x_discrete(expand=c(0.1, 0)) +
+    scale_y_continuous( breaks = y_breaks, labels = y_labels, limits = c(-max_y, 0) ) +
     geom_line( aes( group = id, colour = sign(slope) * sqrt( abs(slope) ), alpha = 1 ), size = 1) +
     geom_point( aes(fill = transient), shape = 21, color="transparent" , size = 2 ) +
     scale_color_gradient2( limits = c(-6, 6), mid=get_background_color(), high=muted("blue"), low=muted("red"))  +
     scale_fill_manual( values=c("transparent","black") )
 
+  plot <- plot + ggtitle(first(data$measure))
+  
   return(plot)
   
 }
@@ -21,11 +28,14 @@ diff_plot <- function(data, debug = F ) {
 adjust_theme <- function( debug ){
   
   if(debug){
-    plot_theme <- theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    plot_theme <- theme(axis.text.x = element_text(angle = 45, hjust = 1)
+                        ,plot.background = element_rect(fill = "white", colour ="white")
+                        ,panel.background = element_rect(fill=get_background_color())
+                        ,panel.grid.minor = element_blank()
+                        ,legend.position = "none"
+                        ,axis.title.x = element_blank()
+                        ,axis.title.y = element_blank())
   } else {
-
-    
-    
     plot_theme <- theme_bw() +
        theme(
          plot.background = element_rect(fill = "white", colour ="white")
@@ -55,4 +65,12 @@ adjust_theme <- function( debug ){
 get_background_color <- function() {
   background.color <- rgb(240,240,240,maxColorValue=255)
   return(background.color)
+}
+
+#split the data by measure and create a plot for each
+diff_plot_grid <- function(data, debug = F ) {
+  
+  plots <- dlply( data, .(measure), function(x) diff_plot(x, debug) )
+  return(plots)
+  
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 I wrote this package to visually track changes in rankings over time. It's original purpose was to help with search engine tuning.  By monitoring the search rankings of products over time on ecommerce sites, you can understand their volatility and what factors (such as search tuning) can influence these rankings.  
 
-Here is an example of what this looks like:  This plot shows hourly changes in the top 50 search results for "home styles" on a leading ecommerce site, for a period of 100 hours. 
+Here is an example of what this looks like:  This plot shows dialy changes in the top 50 search results for 9 different search terms on a leading ecommerce site, for a period of 10 days. 
 
 ![example plot](/img/example.png?raw=true)
 
@@ -11,3 +11,7 @@ Different visual encodings can be used to emphasize aspects of the data.  The de
  - intensity to the degree of change
  - color to the direction of change
  - black dots are used to show results that appear or disappear from the rankings
+
+This visual encoding can give you a sense of ranking changes even when the view is compact:
+
+![example compact plot](/img/example_small_scale.png?raw=true)
